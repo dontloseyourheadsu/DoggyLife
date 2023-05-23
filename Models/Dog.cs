80561fc41a8a542f7680ab9ba901d61e34f7715f
dontloseyourheadsu <93137954+dontloseyourheadsu@users.noreply.github.com>
2023-05-23 12:08:24 -0600

    Added walk method base and boundaries base

--- Models/Dog.cs
+++ Models/Dog.cs
@@ -4,4 +4,19 @@ public class Dog
 {
     public float X { get; set; }
     public float Y { get; set; }
+
+	public List<List<float>> Boundaries = new() 
+	{
+		
+	}
+
+	public async void WalkAsync()
+    {
+        while (true)
+        {
+            await Task.Delay(1000);
+            X += 1;
+            Y += 1;
+        }
+    }
 }
