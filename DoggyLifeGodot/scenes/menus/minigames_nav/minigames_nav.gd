0788ba7c78827e9bc6d9429f87ac3c92df520b15
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-10-31 13:02:00 -0600

    Navigate to catch the ball minigame

--- DoggyLifeGodot/scenes/menus/minigames_nav/minigames_nav.gd
+++ DoggyLifeGodot/scenes/menus/minigames_nav/minigames_nav.gd
@@ -3,8 +3,26 @@ extends Control
 func _ready():
 	pass
 
+# Go back to room scene
 func _on_button_pressed() -> void:
 	var room_scene = load("res://scenes/room/room.tscn").instantiate()
 	get_tree().root.add_child(room_scene)
 	get_tree().current_scene.queue_free()
 	get_tree().current_scene = room_scene
+
+
+func _on_minigames_list_item_selected(index: int) -> void:
+	var scene
+	
+	print_debug(index)
+	
+	# Select scene to play
+	if index == 0:
+		scene = load("res://scenes/catch-the-ball/catch_ball_miningame.tscn").instantiate()
+	else:
+		return
+	
+	# switch to scene
+	get_tree().root.add_child(scene)
+	get_tree().current_scene.queue_free()
+	get_tree().current_scene = scene
